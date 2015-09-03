77e49da0e23ca8b8a7012df9ff3eddd430925d74
Michael Pollett <mpollett@rmn.com>
2015-09-04 00:30:04 +0100

    clean deps

--- build.sbt
+++ build.sbt
@@ -5,6 +5,5 @@ version := "1.0"
 scalaVersion := "2.11.7"
 
 libraryDependencies ++= Seq(
-  "net.sourceforge.htmlcleaner" % "htmlcleaner" % "2.6.1",
   "net.sourceforge.htmlunit" % "htmlunit" % "2.18"
 )
--- src/main/scala/uk/co/pollett/scala/Main.scala
+++ src/main/scala/uk/co/pollett/scala/Main.scala
@@ -2,24 +2,12 @@ package uk.co.pollett.scala
 
 import java.net.URL
 
-import com.gargoylesoftware.htmlunit.html.{DomAttr, DomNode, HtmlPage, HtmlElement}
-import com.gargoylesoftware.htmlunit.{SgmlPage, BrowserVersion, WebClient}
+import com.gargoylesoftware.htmlunit.html.{DomNode, HtmlElement, HtmlPage}
+import com.gargoylesoftware.htmlunit.{BrowserVersion, WebClient}
 
 import scala.collection.JavaConversions._
 
 object Main {
-
-  implicit class HtmlElementCompare(e: HtmlElement){
-    def depth: Int = {
-      var dom: DomNode = e.getParentNode
-      var i = 0
-      while(dom != null){
-        i=i+1
-        dom = dom.getParentNode
-      }
-      i
-    }
-  }
 
   def main(args: Array[String]) {
 
@@ -41,13 +29,25 @@ object Main {
       val content = el.getTextContent
       if (price_reg.findAllIn(content).nonEmpty) {
         val price = price_reg.findFirstIn(el.getTextContent).get
-        if(!price.equals("£0")) {
+        if (!price.equals("£0")) {
           println("distance: " + el.depth)
           println(el.getAttribute("class"))
           println(price)
           println("--")
         }
       }
+    }
+  }
+
+  implicit class HtmlElementCompare(e: HtmlElement) {
+    def depth: Int = {
+      var dom: DomNode = e.getParentNode
+      var i = 0
+      while (dom != null) {
+        i = i + 1
+        dom = dom.getParentNode
+      }
+      i
     }
   }
 }
